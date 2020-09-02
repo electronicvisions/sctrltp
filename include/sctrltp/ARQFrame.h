311a13067d2d4068534522cb6fd5b024ee5830f8
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2020-09-02 18:58:54 +0200

    clang-format: include/sctrltp/ARQFrame.h
    Change-Id: I91900edd89da79e2acb7199f18684453b543393c

--- include/sctrltp/ARQFrame.h
+++ include/sctrltp/ARQFrame.h
@@ -6,36 +6,37 @@
 namespace sctrltp {
 
 /* standard packet type: with payload */
-template<typename P>
-struct packet {
-    typedef uint32_t seq_t;
-    typedef uint64_t entry_t;
+template <typename P>
+struct packet
+{
+	typedef uint32_t seq_t;
+	typedef uint64_t entry_t;
 
-    seq_t ack;
-    seq_t seq;
-    packetid_t pid;
-    uint16_t len;
+	seq_t ack;
+	seq_t seq;
+	packetid_t pid;
+	uint16_t len;
 
-    union {
-        entry_t pdu[P::MAX_PDUWORDS];
-        uint8_t rawpdu[P::MAX_PDUWORDS*8];
-    };
-    static_assert(sizeof(pdu) == sizeof(rawpdu), "");
+	union
+	{
+		entry_t pdu[P::MAX_PDUWORDS];
+		uint8_t rawpdu[P::MAX_PDUWORDS * 8];
+	};
+	static_assert(sizeof(pdu) == sizeof(rawpdu), "");
 
-    size_t size() const {
-        size_t tmp = 0;
-        tmp += sizeof(ack);
-        tmp += sizeof(seq);
-        tmp += sizeof(pid);
-        tmp += sizeof(len);
-        tmp += len * 8; // length is 64bit-wise!
-        return tmp;
-    }
+	size_t size() const
+	{
+		size_t tmp = 0;
+		tmp += sizeof(ack);
+		tmp += sizeof(seq);
+		tmp += sizeof(pid);
+		tmp += sizeof(len);
+		tmp += len * 8; // length is 64bit-wise!
+		return tmp;
+	}
 
-    packet() :
-        pid(0xDEAD),
-        len(1) // minimum length
-    {}
-} __attribute__ ((__packed__));
+	packet() : pid(0xDEAD), len(1) // minimum length
+	{}
+} __attribute__((__packed__));
 
 } // namespace sctrltp
