e53f94e3a53dd94949eb6242b052090428d8c906
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2026-03-17 15:57:23 +0100

    chore: Adapt to c/26335
    Depends-On: 26335
    Change-Id: I5870b43537083d0a95b0f7c4931cdc9bc18770c6

--- include/sctrltp/logger.h
+++ include/sctrltp/logger.h
@@ -21,9 +21,7 @@
 		fprintf(stderr, "ERROR %s:%d: ", __FILE__, __LINE__);                                      \
 		fprintf(stderr, __VA_ARGS__);                                                              \
 		fprintf(stderr, "\n");                                                                     \
-		char buffer[256];                                                                          \
-		snprintf(buffer, sizeof(buffer), __VA_ARGS__);                                             \
-		LOGGER_OPEN_SYSLOG_CLOSE(ERROR, "sctrltp", buffer);                                        \
+		LOGGER_OPEN_SYSLOG_CLOSE(ERROR, "sctrltp", __VA_ARGS__);                                   \
 	} while (0)
 #if LOGLEVEL > 0
 #define SCTRL_LOG_WARN(...)                                                                        \
